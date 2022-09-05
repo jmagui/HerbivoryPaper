76a14459c4b33495b6a8c1b461153f73a1141c53
jmagui <53578934+jmagui@users.noreply.github.com>
2022-09-05 10:13:39 -0700

    Update HerbivoryPaper_Gut_Mouthhook.R

--- HerbivoryPaper_Gut_Mouthhook.R
+++ HerbivoryPaper_Gut_Mouthhook.R
@@ -1,28 +1,30 @@
+#install packages
+install.packages("ggplot2")
+install.packages("tidyverse")
+install.packages("dbplyr")
+
+#load packages
 library(ggplot2)
 library(tidyverse)
 library(dbplyr)
 
-getwd()
-
-setwd("/Users/jessicaaguilar/Desktop/Temporary.data.folder")
+setwd("~/Library/CloudStorage/Box-Box/Herbivory_paper/HerbivoryPaperRepository")
 
 data <- read.csv('gut_mouthhook_cleaned_2022.02.22.csv')
-
-
 shape_data<- read.csv("mouthookshape_area_2022.03.23.csv")
 
 
+#divide measurements by body size 
 data2 <-
   mutate(data,
          gut_body = gut/body,
          skeleton_body = skeleton/body
   )
 
+
 #plots for mouthhook and gut sizes, shapes 
 
-
 ##gut length / body
-
 gut.plot <- ggplot(data = data2) +
   theme_classic() +
   geom_boxplot(aes(x= species, y= gut_body)) +
@@ -33,12 +35,14 @@ gut.plot <- ggplot(data = data2) +
 
 gut.plot
 
+##gut length/body anova + TukeysHSD
 gut.aov <- aov(gut_body ~ species, data = data2)
 summary(gut.aov)
 TukeyHSD(gut.aov)
 
-##cephaloskeleton/body 
 
+
+##cephaloskeleton/body 
 skeleton.plot <- ggplot(data = data2) +
   theme_classic() +
   geom_boxplot(aes(x= species, y= skeleton_body)) +
@@ -46,15 +50,16 @@ skeleton.plot <- ggplot(data = data2) +
   ylab("cephaloskeleton/body") +
   xlab("species") +
   geom_point(aes(x= species, y= skeleton_body), position = position_jitter(w = 0.2, h = 0))
-
 skeleton.plot
 
+##cephaloskeleton/body anova + TurkeysHSD
 skeleton.aov <- aov(skeleton_body ~ species, data = data2)
 summary(skeleton.aov)
 TukeyHSD(skeleton.aov)
 
-##mouthhook height/ length
 
+
+##mouthhook height/ length plot
 mh_base_ratio.plot <- ggplot(data = shape_data) +
   theme_classic() +
   geom_boxplot(aes(x= species, y= bottomlength_baseheight_ratio)) +
@@ -62,18 +67,16 @@ mh_base_ratio.plot <- ggplot(data = shape_data) +
   ylab("bottom height / base length") +
   xlab("species") +
   geom_point(aes(x= species, y= bottomlength_baseheight_ratio), position = position_jitter(w = 0.2, h = 0))
-
 mh_base_ratio.plot
 
-
+##mouthhook height/ length anova + TurkeysHSD
 mh_base_ratio.aov <- aov(bottomlength_baseheight_ratio ~ species, data = shape_data)
 summary(mh_base_ratio.aov)
 TukeyHSD(mh_base_ratio.aov)
 
 
 
-##toothsize 
-
+##toothsize plot
 toothsize.plot <- ggplot(data = shape_data) +
   theme_classic() +
   geom_boxplot(aes(x= species, y= tooth_size)) +
@@ -84,6 +87,7 @@ toothsize.plot <- ggplot(data = shape_data) +
 
 toothsize.plot
 
+##toothsize anova + TurkeysHSD
 toothsize.aov <- aov(tooth_size ~ species, data = shape_data)
 summary(toothsize.aov)
 TukeyHSD(toothsize.aov)
